94e74e1cdaefb8284f1d26ab5f42423fe8ca7e5b
Romina Suarez <rominasuarezsh@gmail.com>
2022-05-24 20:09:27 -0300

    Creamos la funcion de suma

---
+++ main.py
@@ -0,0 +1,11 @@
+#Suarez Romina Marcela
+number1 = int(input('introducce el primer numero '))
+number2 = int(input('introducce el segundo numero '))
+number3 = int(input('introducce el tecer numero '))
+result = 0
+
+def suma (a,b,c):return print(a+b+c)
+
+
+result = suma(number1,number2,number3)
+ 
