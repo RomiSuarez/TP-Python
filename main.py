c78fc786f08b0a26749b3a0ec52f3cc30e1e2531
Romina Suarez <rominasuarezsh@gmail.com>
2022-05-24 20:27:27 -0300

    Creamos la Funcion Resta

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 number1 = int(input('introducce el primer numero '))
 number2 = int(input('introducce el segundo numero '))
 number3 = int(input('introducce el tecer numero '))
-result = 0
+result =0
 
-def suma (a,b,c):return print(a+b+c)
+##1-suma
+##def suma (a,b,c):return print(a+b+c)
 
 
-result = suma(number1,number2,number3)
- 
+##result = suma(number1,number2,number3)
+def resta (a,b,c):return a-b-c
+result = resta(number1,number2,number3)
+print(result)
